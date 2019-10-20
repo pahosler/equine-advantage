bc3ce4695ca973272e895bb96dba406e868413bd
pahosler <pahosler@gmail.com>
2019-10-19 19:11:39 -0500

    commit

---
+++ src/components/StoreLocator/index.js
@@ -0,0 +1,11 @@
+// import React, { useState, useEffect } from 'react'
+import {
+  withGoogleMap,
+  withScriptjs,
+  GoogleMap,
+  Marker,
+  InfoWindow,
+} from 'react-google-maps'
+
+import * as locations from '../../../static/admin/locations.json'
+
